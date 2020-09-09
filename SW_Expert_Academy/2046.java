8dcba058df2ca9e6222544d0e900f806bae0485f
장하영 <hyj9829@naver.com>
2020-09-09 16:32:38 +0900

    스탬프 찍기

---
+++ SW_Expert_Academy/2046.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+import java.io.FileInputStream;
+
+class Solution
+{
+	public static void main(String args[]) throws Exception
+	{
+		Scanner sc = new Scanner(System.in);
+		int T;
+		T=sc.nextInt();
+
+		for(int test_case = 1; test_case <= T; test_case++)
+		{
+            System.out.print("#");
+		}
+	}
+}
