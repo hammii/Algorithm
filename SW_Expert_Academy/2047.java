8f703ce45ebab9062a525e626273ba3b1f38d973
장하영 <hyj9829@naver.com>
2020-09-09 16:23:43 +0900

    신문 헤드라인

---
+++ SW_Expert_Academy/2047.java
@@ -0,0 +1,13 @@
+package com.gachon;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+
+        System.out.println(str.toUpperCase());
+    }
+}
