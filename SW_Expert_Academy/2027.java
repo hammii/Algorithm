77c9929dc73a66ec341a141ca2e2b8ca88b9f4d9
장하영 <hyj9829@naver.com>
2020-09-11 23:52:02 +0900

    대각선 출력하기

---
+++ SW_Expert_Academy/2027.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+import java.io.FileInputStream;
+
+class Solution
+{
+	public static void main(String args[]) throws Exception
+	{
+        for(int i=0;i<5;i++){
+            for(int j=0;j<5;j++){
+                if(i==j){
+                    System.out.print("#");
+                }else{
+                    System.out.print("+");
+                }
+            }
+            System.out.println("");
+        }
+	}
+}
