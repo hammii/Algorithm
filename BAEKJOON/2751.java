16282b4816c303438772b5985cd6a97ef1d71733
장하영 <hyj9829@naver.com>
2021-01-11 20:16:36 +0900

    수 정렬하기 2

---
+++ BAEKJOON/2751.java
@@ -0,0 +1,28 @@
+package com.gachon;
+
+import java.io.*;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+
+        Arrays.sort(arr);
+
+        for (int i = 0; i < N; i++) {
+            bw.write(arr[i] + "\n");
+        }
+
+        br.close();
+        bw.flush();
+        bw.close();
+    }
+}
