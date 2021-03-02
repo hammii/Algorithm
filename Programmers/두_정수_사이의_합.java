1a5fa562a82103c8cd55bc89f1a56e11f89dff36
장하영 <hyj9829@naver.com>
2021-03-02 23:35:29 +0900

    Create 두_정수_사이의_합.java

---
+++ Programmers/두_정수_사이의_합.java
@@ -0,0 +1,15 @@
+class Solution {
+  public long solution(int a, int b) {
+      long answer = 0;
+
+      if(a==b) return (long) a;
+
+      int max = Math.max(a,b);
+      int min = Math.min(a,b);
+      for(int i = min; i <= max; i++){
+          answer += i;
+      }
+
+      return answer;
+  }
+}
