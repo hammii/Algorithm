585159e2b02a0f157c81dbc482f5220b0171b564
장하영 <hyj9829@naver.com>
2021-03-12 15:36:53 +0900

    Update H-Index.java

--- Programmers/H-Index.java
+++ Programmers/H-Index.java
@@ -1,24 +1,16 @@
 import java.io.*;
 import java.util.Arrays;
 
-public class Main {
-    public static void main(String[] args) throws IOException {
-        System.out.println(solution(new int[]{6, 5, 4, 1, 0}));
-    }
-
-    public static int solution(int[] citations) {
+class Solution {
+    public int solution(int[] citations) {
         int answer = 0;
 
         Arrays.sort(citations);
-        int max = citations[citations.length - 1];
-        int idx = citations.length - 1;
-
-        for (int h = max; h >= 0; h--) {
-            if (h >= citations[idx] && citations.length - idx <= h) {
-                answer = h;
+        for (int i = citations.length - 1; i >= 0; i--) {
+            if (citations[i] <= answer) {
                 break;
             }
-            if (Arrays.asList(citations).contains(h)) idx--;
+            answer++;
         }
 
         return answer;
