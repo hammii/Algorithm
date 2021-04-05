da08999ded2a425325a6976ec2489468f08a71e3
장하영 <hyj9829@naver.com>
2021-04-05 16:12:10 +0900

    Create 이중우선순위큐.java

---
+++ Programmers/이중우선순위큐.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(String[] operations) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>();  // 오름차순
+        PriorityQueue<Integer> reverse_pq = new PriorityQueue<>(Comparator.reverseOrder()); // 내림차순
+        int[] answer = new int[2];
+
+        for (int i = 0; i < operations.length; i++) {
+            String oper = operations[i].split(" ")[0];
+            int num = Integer.parseInt(operations[i].split(" ")[1]);
+
+            if (oper.equals("I")) {
+                pq.add(num);
+                reverse_pq.add(num);
+            } else if (oper.equals("D")) {
+                if (num == 1) { // 최댓값 삭제
+                    Integer max = reverse_pq.peek();
+                    pq.remove(max);
+                    reverse_pq.remove(max);
+                } else {    // 최솟값 삭제
+                    Integer min = pq.peek();
+                    pq.remove(min);
+                    reverse_pq.remove(min);
+                }
+            }
+        }
+
+        if (!pq.isEmpty()) {
+            answer[0] = reverse_pq.peek();
+            answer[1] = pq.peek();
+        }
+
+        return answer;
+    }
+}
