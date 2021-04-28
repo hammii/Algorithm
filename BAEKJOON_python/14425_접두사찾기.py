e96bcc28673f6e44244ca12736fde05cf1466bd6
장하영 <hyj9829@naver.com>
2021-04-29 02:10:45 +0900

    Update 14425_접두사찾기.py

--- BAEKJOON_python/14425_접두사찾기.py
+++ BAEKJOON_python/14425_접두사찾기.py
@@ -1,14 +1,11 @@
 import sys
 
 N, M = map(int, sys.stdin.readline().rstrip().split())
-
-strings = []
-for i in range(N):
-    strings.append(input())
+strings = [sys.stdin.readline().strip() for _ in range(N)]
 
 cnt = 0
 for _ in range(M):
-    word = input()
+    word = sys.stdin.readline().strip()
     for string in strings:
         if string.startswith(word):
             cnt += 1
