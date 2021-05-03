2bbbd3a8f6cc3ba832879d121313f84be8dd28b9
장하영 <hyj9829@naver.com>
2021-05-03 20:17:18 +0900

    Update 1201_보석도둑.py

--- BAEKJOON_python/1201_보석도둑.py
+++ BAEKJOON_python/1201_보석도둑.py
@@ -1,22 +1,28 @@
 import sys
+import heapq
 input = sys.stdin.readline
 
 N, K = map(int, input().rstrip().split())
 jewelry = []
-bag = []
+bags = []
 
 for _ in range(N):
-    M, V = map(int, input().rstrip().split())
-    jewelry.append([M, V])
+    M, V = map(int, input().rstrip().split())  # 무게와 가격
+    heapq.heappush(jewelry, [M, V])
 
-for _ in range(K):
-    bag.append(int(input().rstrip()))
+bags = [int(input().rstrip()) for _ in range(K)]
+bags.sort()  # 가벼운 가방부터 정렬
 
-jewelry = sorted(jewelry, key=lambda x: (-x[1], x[0]))  # 비싼 보석부터 정렬
-bag = sorted(bag)  # 가벼운 가방부터 정렬
-
-j_idx = 0
 answer = 0
+temp_j = []
 
+for bag in bags:
+    while jewelry and jewelry[0][0] <= bag:
+        heapq.heappush(temp_j, -jewelry[0][1])  # 가격과 주소 우선순위큐에 저장
+        heapq.heappop(jewelry)
+    if temp_j:
+        answer -= heapq.heappop(temp_j)
+    elif not jewelry:
+        break
 
 print(answer)
