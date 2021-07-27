d0524e3a932d0548e411fb9773f76bfe5dcbff6d
장하영 <hyj9829@naver.com>
2021-07-27 12:37:49 +0900

    Create 1715_카드정렬하기.py

---
+++ BAEKJOON_python/1715_카드정렬하기.py
@@ -0,0 +1,21 @@
+import sys
+import heapq
+
+N = int(sys.stdin.readline().rstrip())
+num = [int(sys.stdin.readline().rstrip()) for _ in range(N)]
+
+pq = []
+ans = 0
+
+for n in num:
+    heapq.heappush(pq, n)
+
+while len(pq) >= 2:
+    n1 = heapq.heappop(pq)
+    n2 = heapq.heappop(pq)
+    ans += n1 + n2
+
+    if len(pq) != 0:
+        heapq.heappush(pq, n1 + n2)
+
+print(ans)
