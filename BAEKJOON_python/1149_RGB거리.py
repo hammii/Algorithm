21640d3664f9aec06c22de474c14860d8ce504e6
장하영 <hyj9829@naver.com>
2021-07-30 22:31:29 +0900

    Create 1149_RGB거리.py

---
+++ BAEKJOON_python/1149_RGB거리.py
@@ -0,0 +1,14 @@
+import sys
+
+N = int(sys.stdin.readline().rstrip())
+cost = [list(map(int, sys.stdin.readline().rstrip().split())) for _ in range(N)]
+ans = int(1e9)
+dp = [[0] * 3 for _ in range(N)]
+
+dp[0] = cost[0]
+for i in range(1, N):
+    dp[i][0] = min(dp[i - 1][1] + cost[i][0], dp[i - 1][2] + cost[i][0])
+    dp[i][1] = min(dp[i - 1][0] + cost[i][1], dp[i - 1][2] + cost[i][1])
+    dp[i][2] = min(dp[i - 1][0] + cost[i][2], dp[i - 1][1] + cost[i][2])
+
+print(min(dp[N - 1][0], dp[N - 1][1], dp[N - 1][2]))
