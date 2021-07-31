2083d8f89f47f716a12fc537e8b4265e23bfe2b1
장하영 <hyj9829@naver.com>
2021-07-31 23:27:21 +0900

    Create 12865_평범한배낭.py

---
+++ BAEKJOON_python/12865_평범한배낭.py
@@ -0,0 +1,22 @@
+import sys
+
+N, K = map(int, sys.stdin.readline().rstrip().split())
+w_list = []
+v_list = []
+dp = [[0 for _ in range(K + 1)] for _ in range(N + 1)]
+
+for _ in range(N):
+    W, V = map(int, sys.stdin.readline().rstrip().split())
+    w_list.append(W)
+    v_list.append(V)
+
+for i in range(1, N + 1):
+    for w in range(1, K + 1):
+        if i == 0 or w == 0:
+            dp[i][w] = 0
+        elif w_list[i - 1] <= w:
+            dp[i][w] = max(v_list[i - 1] + dp[i - 1][w - w_list[i - 1]], dp[i - 1][w])
+        else:
+            dp[i][w] = dp[i - 1][w]
+
+print(dp[N][K])
