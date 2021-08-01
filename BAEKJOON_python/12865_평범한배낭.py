f4543779be236bdb135ca830fa217d95a1d18d75
장하영 <hyj9829@naver.com>
2021-08-01 11:39:50 +0900

    Update 12865_평범한배낭.py

--- BAEKJOON_python/12865_평범한배낭.py
+++ BAEKJOON_python/12865_평범한배낭.py
@@ -1,8 +1,8 @@
 import sys
 
 N, K = map(int, sys.stdin.readline().rstrip().split())
-w_list = []
-v_list = []
+w_list = [0]
+v_list = [0]
 dp = [[0 for _ in range(K + 1)] for _ in range(N + 1)]
 
 for _ in range(N):
@@ -14,8 +14,8 @@ for i in range(1, N + 1):
     for w in range(1, K + 1):
         if i == 0 or w == 0:
             dp[i][w] = 0
-        elif w_list[i - 1] <= w:
-            dp[i][w] = max(v_list[i - 1] + dp[i - 1][w - w_list[i - 1]], dp[i - 1][w])
+        elif w_list[i] <= w:
+            dp[i][w] = max(v_list[i] + dp[i - 1][w - w_list[i]], dp[i - 1][w])
         else:
             dp[i][w] = dp[i - 1][w]
 
