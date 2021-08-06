f6911964d645a17661536991bdd5e4c33ea46f04
장하영 <hyj9829@naver.com>
2021-08-07 02:01:56 +0900

    Update 편집거리.py

--- python_practice/다이나믹프로그래밍/편집거리.py
+++ python_practice/다이나믹프로그래밍/편집거리.py
@@ -3,20 +3,21 @@ import sys
 
 origin = list(sys.stdin.readline().rstrip())
 change = list(sys.stdin.readline().rstrip())
+n = len(origin)
+m = len(change)
 
-dp = [[0] * (len(change) + 1) for _ in range(len(origin) + 1)]
+dp = [[0] * (m + 1) for _ in range(n + 1)]
 
-for i in range(1, len(origin) + 1):
+for i in range(1, n + 1):
     dp[i][0] = i
-
-for j in range(1, len(change) + 1):
+for j in range(1, m + 1):
     dp[0][j] = j
 
-for i in range(1, len(origin) + 1):
-    for j in range(1, len(change) + 1):
+for i in range(1, n + 1):
+    for j in range(1, m + 1):
         if origin[i - 1] == change[j - 1]:
             dp[i][j] = dp[i - 1][j - 1]
         else:
             dp[i][j] = min(dp[i - 1][j], dp[i][j - 1], dp[i - 1][j - 1]) + 1
 
-print(dp[len(origin) - 1][len(change) - 1])
+print(dp[n][m])
