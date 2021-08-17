a67fd14b0ffda5ed909a6c60ab2363579cc2f9ec
장하영 <hyj9829@naver.com>
2021-08-17 11:20:33 +0900

    Create 숫자_문자열과_영단어.py

---
+++ Programmers_python/숫자_문자열과_영단어.py
@@ -0,0 +1,18 @@
+num = {'zero': 0, 'one': 1, 'two': 2,
+       'three': 3, 'four': 4, 'five': 5,
+       'six': 6, 'seven': 7, 'eight': 8, 'nine': 9}
+
+
+def solution(s):
+    answer = ''
+    temp = ''
+
+    for i in s:
+        if 'a' <= i <= 'z':
+            temp += i
+            if temp in num:
+                answer += str(num[temp])
+                temp = ''
+        else:
+            answer += i
+    return int(answer)
