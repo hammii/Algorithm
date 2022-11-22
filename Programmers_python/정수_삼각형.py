c324ec9af5605530a6a9a143cc16761ea10640bc
HaYeong Jang <hyj9829@naver.com>
2022-11-22 13:20:13 +0900

    Create 정수_삼각형.py

---
+++ Programmers_python/정수_삼각형.py
@@ -0,0 +1,15 @@
+def solution(triangle):
+    answer = 0
+    dp = [[0] * len(triangle[-1]) for _ in range(len(triangle))]
+    dp[0][0] = triangle[0][0]
+    
+    for i in range(1, len(triangle)):
+        for j in range(len(triangle[i])):
+            if j == 0:
+                dp[i][j] = dp[i-1][j] + triangle[i][j]
+            elif j == i:
+                dp[i][j] = dp[i-1][j-1] + triangle[i][j]
+            else:
+                dp[i][j] = max(dp[i-1][j-1], dp[i-1][j]) + triangle[i][j]
+                
+    return max(dp[len(triangle)-1])
