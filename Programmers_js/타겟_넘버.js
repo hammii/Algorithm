3023269df454a0fb6aace8f42bd6e6c7f6fae72a
HaYeong Jang <hyj9829@naver.com>
2023-10-21 17:38:10 +0900

    Create 타겟_넘버.js

---
+++ Programmers_js/타겟_넘버.js
@@ -0,0 +1,18 @@
+function solution(numbers, target) {
+    let answer = 0;
+    
+    function dfs(idx, total) {
+        if(idx === numbers.length) {
+            if(total === target) {
+                answer += 1;
+            }
+            return;
+        }
+
+        dfs(idx+1, total+numbers[idx]);
+        dfs(idx+1, total-numbers[idx]);
+    }
+    
+    dfs(0, 0);
+    return answer;
+}
